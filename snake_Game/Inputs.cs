3656caf6c8e5dcdb9e93187ac0e1df4f1a69f239
muhammadsaqib051994 <maliksaqib051994@gmail.com>
2015-05-11 01:43:05 -0700

    utton Pressing Check

--- snake_Game/Inputs.cs
+++ snake_Game/Inputs.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.Collections;
+using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace snake_Game
 {
     class Inputs
     {
            private static Hashtable keytable = new Hashtable();
-           public static bool KeyPressed(Keys key);
-
-
- 
-
-
+           public static bool keyPressed(Keys key)
+           
+            {
+               if(keytable[key]==null)
+            
+                  {
+                        return false; 
+                  }   
+            } 
     }
 }
